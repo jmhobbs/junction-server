785888cc5135f96ba18b475f185a5f335f16376f
John Hobbs <john@velvetcache.org>
2014-11-18 22:43:40 -0600

    v0

---
+++ index.js
@@ -0,0 +1,18 @@
+// https://github.com/Automattic/socket.io/tree/0.9.17
+var http = require('http'),
+    http_server = http.createServer(http_reponse),
+    io = require('socket.io').listen(http_server),
+		url = require('url');
+
+function http_reponse (request, response) {
+	var parsed_url = url.parse(request.url, true);
+  io.sockets.emit("message", {"message": parsed_url.query.message, "title": parsed_url.query.title});
+  response.writeHead(200, {'Content-Type': 'text/json'});
+  response.end(JSON.stringify({"response": "rad dude"}));
+}
+
+io.sockets.on('connection', function (socket) {
+  console.log("client connected");
+});
+
+http_server.listen(process.env.PORT || 3000);
